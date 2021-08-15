64591b055c4cc815011326974ca0ea656e587e5a
Joseph Wang <44278515+EmeraldEntities@users.noreply.github.com>
2021-08-14 22:10:49 -0400

    home done or something

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './home.css';
 
 const Home: React.FC = () => {
   return (
-    <div>
-      <p>This is the home page</p>
+    <div className="main-container">
+      <div className="main-section">
+        <div className="main-text-container">
+          <h1>Productivity Apps Failing?</h1>
+          <p className="main-text-container-sentence">
+            Finally, an app that does it
+          </p>
+          <p className="main-text-container-declaration">The right way.</p>
+
+          {/* i dunno why putting this outside this div doesn't work lol */}
+          <div className="outer-link-wrapper">
+            <Link to="/dashboard" className="hide-link">
+              <div className="main-button">
+                <p className="button-text">get started</p>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      <div className="about-section">
+        <p>despacito</p>
+      </div>
     </div>
   );
 };
