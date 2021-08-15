5a5dbfac497d1590cd4536aa84f80ff78e9c070d
Joseph Wang <44278515+EmeraldEntities@users.noreply.github.com>
2021-08-15 03:26:53 -0400

    yap tapp nav bap

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navigation: React.FC = () => {
-    return <div></div>
-}
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <Link to="/">
+          <img src="./assets/yaptappp.PNG" className="icon" />
+        </Link>
+      </div>
+      <div className="links">
+        <ul>
+          <li>about us</li>
+          <li>our science</li>
+          <Link to="/dashboard">
+            <li>get started</li>
+          </Link>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Navigation;
